0877875c2e0cdf0b91d945e19f29585b7dab8897
Zac Willmington <zac.willmington@gmail.com>
2018-04-26 22:20:47 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
 class Actor < ActiveRecord::Base
-  
+  belongs_to :shows
+  has_many :characters, through: :shows
+
+  def full_name
+        "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #   binding.pry
+    #   id = self.show_id
+    #   binding.pry
+    #   show_name = Show.find_by(id).name
+    #   "#{self.characters.name} - #{show_name}"
+    #   binding.pry
+    role = []
+      self.characters.all.each do |char|
+          binding.pry
+          role << "#{char.name} - #{Show.find_by(char.show_id)}"
+      end
+      role
+      binding.pry
+  end
 end
---
+++ db/migrate/008_add_id_to_shows.rb
@@ -0,0 +1,5 @@
+class AddIdToShows < ActiveRecord::Migration[4.2]
+  def change
+      add_column :shows, :character_id, :integer
+  end
+end
